03be5d51dd6d0b7035fa16d1f2af8cd936685ed7
Halceeyon <IsaacAki247@gmail.com>
2023-03-21 16:03:36 +0000

    Revert "Firebase Implementation and small homepage changes"
    This reverts commit a7f21d74deac06b46e4c25b64e8617dea5cc86cd.

--- dog_tinder/macos/Flutter/GeneratedPluginRegistrant.swift
+++ dog_tinder/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import firebase_core
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
 }
