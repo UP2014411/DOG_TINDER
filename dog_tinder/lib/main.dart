b81e0b42dbcca2a27f4a85490b8d630c0eebbdd5
Halceeyon <IsaacAki247@gmail.com>
2023-03-27 12:02:19 +0100

    Created basic interface layout in main.dart
    - Added firebase code for when we are ready to create the database. Currently commented out as it messes up the app causing it to not run when it is there.
    
    - Deleted StatelessWidget and created StatefulWidget
    
    - Created App Bar that displays the name of the app and will also display the name of the pages that the user is currently on
    
    - Changed the background colour
    
    - Created the bottom navigation bar and added the home, chat and account page buttons to it. The buttons work but they need to be mapped to their respective pages
    
    - Created the _selectedInt variable and the _onItemTapped function that allows for the buttons to be pressed and the ability to access the pages

--- dog_tinder/lib/main.dart
+++ dog_tinder/lib/main.dart
@@ -3,126 +3,84 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
 
-  runApp(const MaterialApp(
-    title: 'Tinder for Dogs',
-    home: MyApp(),
-    debugShowCheckedModeBanner: false,
-  ));
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 255, 183, 0),
-        title: const Text(
-          'Tinder for Dogs',
-          style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
-        ),
-        // Placeholer icon code below, thinking of adding a dog head there that also serves as a navigation bar
-        leading: IconButton(
-          icon: const Icon(Icons.add_alert),
-          tooltip: 'Show Snackbar',
-          onPressed: () {
-            ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(content: Text('This is a snackbar')));
-          },
-        ),
-      ),
-      body: Container(),
-    );
+  State<StatefulWidget> createState() {
+    return MyAppState();
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return MaterialApp(
+      title: 'Tinder for Dogs',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+            elevation: 5, // 0 to remove shadow
+            backgroundColor: Color.fromARGB(255, 255, 60, 0),
+            titleTextStyle: TextStyle(fontFamily: 'Rubik', fontSize: 20)),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+        // Top App Bar
+        appBar: AppBar(
+          title: const Text(
+            '🐶 Tinder for Dogs',
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'Rubik',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+        ),
+
+        // App Background
+        body: Stack(
+          children: <Widget>[
+            Container(
+              // App Background Image
+              color: Colors.red[50],
             ),
           ],
         ),
+
+        // Bottom Nav bar
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex, // References what tab the user is on
+          onTap: _onItemTapped, // Changes tab when clicked
+          selectedItemColor: Colors.amber[800],
+          items: const <BottomNavigationBarItem>[
+            // Home Icon
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            // Chat Bubble Icon
+            BottomNavigationBarItem(
+                icon: Icon(Icons.chat_bubble), label: 'Chat'),
+            // Account Icon
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle_rounded), label: 'Account')
+          ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
